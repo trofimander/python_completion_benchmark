df25caaabf46f8b6eca34d5618052bff7ea8b0bf
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2018-11-24 19:03:20 +0300

    Cleanup

--- 11_e_django_deprecated_attr.py
+++ 11_e_django_deprecated_attr.py
@@ -2,4 +2,4 @@ from django.db.models import Manager
 
 # use_for_related_fields was removed in Django 2.0
 
-Manager.use_for_related_fields  # use_for_related_fields
+x = Manager.use_for_related_fields  # NO! use_for_related_fields
